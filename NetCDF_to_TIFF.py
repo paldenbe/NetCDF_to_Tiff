15003b2ac7759765f01ad76deca5721680bdbd3d
paldenbe <151658034+paldenbe@users.noreply.github.com>
2024-03-15 20:20:11 -0400

    Update NetCDF_to_TIFF.py

--- NetCDF_to_TIFF.py
+++ NetCDF_to_TIFF.py
@@ -1,15 +1,12 @@
-####################################
-# Setting up the arcpy environment #
-####################################
 import arcpy
 import os
 
 # Path
-path = r"C:\Paige_GIS\NetCDF_To_Tiff\Data"
+path = r"DATA_FOLDER_PATH"
 arcpy.env.workspace = path
 
 # Set the output geodatabase
-output_geodatabase = r"C:\Paige_GIS\NetCDF_To_Tiff\NetCDF_To_Tiff.gdb"
+output_geodatabase = r"GEODATABASE_PATH"
 
 # To allow overwriting outputs change overwriteOutput option to True.
 arcpy.env.overwriteOutput = False
@@ -28,11 +25,11 @@ for file in nc_list:
     # Construct the full path to the input nc file
     input_nc = os.path.join(path, file)
 
-    # Construct the layer name dynamically based on the file name
-    layer_name = os.path.splitext(file)[0]  # Remove the extension to use as layer name
+    # Construct the layer name based on the file name
+    layer_name = os.path.splitext(file)[0]
 
     # Process: Make NetCDF Raster Layer (Make NetCDF Raster Layer) (md)
-    out_raster_layer = os.path.join(output_geodatabase, layer_name)  # Create a unique name for the output layer
+    out_raster_layer = os.path.join(output_geodatabase, layer_name)
     arcpy.md.MakeNetCDFRasterLayer(input_nc, variable="tasmax_tavg", x_dimension="lon", y_dimension="lat",
                                    out_raster_layer=out_raster_layer)
 
